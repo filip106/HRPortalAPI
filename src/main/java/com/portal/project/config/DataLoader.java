c37fc97aeeac0469c41268c54de107a3121e0c2d
NikolaDjordjevic92 <nikola.djordjevic.92bg@gmail.com>
2020-08-26 17:23:33 +0200

    Added data loader to initialize roles and add 1 user

---
+++ src/main/java/com/portal/project/config/DataLoader.java
@@ -0,0 +1,41 @@
+package com.portal.project.config;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import com.portal.project.model.ApplicationUser;
+import com.portal.project.model.Role;
+import com.portal.project.repository.RoleRepository;
+import com.portal.project.repository.UserRepository;
+
+@Component
+public class DataLoader implements ApplicationRunner {
+
+	@Autowired
+    private UserRepository userRepository;
+	
+	@Autowired
+	private RoleRepository roleRepository;
+
+//    @Autowired
+//    public DataLoader(UserRepository userRepository) {
+//        this.userRepository = userRepository;
+//    }
+    
+    @Override
+    public void run(ApplicationArguments args) {
+    	Role employeeRole = new Role(3L,null,"EMPLOYEE");
+    	roleRepository.save(new Role(1L,null,"ADMIN"));
+    	roleRepository.save(new Role(2L,null,"MANAGER"));
+    	roleRepository.save(employeeRole);
+    	Collection<Role> roles = new ArrayList<>();
+    	roles.add(employeeRole);
+        userRepository.save(new ApplicationUser(1L, "filip", "bot", "filip@bot.com",roles));
+    }
+	
+}
