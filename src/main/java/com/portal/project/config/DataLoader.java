f5e99b2cf3a7c0bd219f54515fd8efa670adfdd3
filip106 <djordjevic.filip994@gmail.com>
2020-08-26 22:21:20 +0200

    added docker

--- src/main/java/com/portal/project/config/DataLoader.java
+++ src/main/java/com/portal/project/config/DataLoader.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.portal.project.model.ApplicationUser;
 import com.portal.project.model.Role;
@@ -22,15 +23,18 @@ public class DataLoader implements ApplicationRunner {
 	@Autowired
 	private RoleRepository roleRepository;
 
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
     @Override
     public void run(ApplicationArguments args) {
-    	Role employeeRole = new Role(3L,null,"EMPLOYEE");
-    	roleRepository.save(new Role(1L,null,"ADMIN"));
-    	roleRepository.save(new Role(2L,null,"MANAGER"));
+    	Role employeeRole = new Role(3L, null, "EMPLOYEE");
+    	roleRepository.save(new Role(1L, null, "ADMIN"));
+    	roleRepository.save(new Role(2L, null, "MANAGER"));
     	roleRepository.save(employeeRole);
     	Collection<Role> roles = new ArrayList<>();
     	roles.add(employeeRole);
-        userRepository.save(new ApplicationUser(1L, "filip", "bot", "filip@bot.com",roles));
+        userRepository.save(new ApplicationUser(1L, "filip", bCryptPasswordEncoder.encode("bot"), "filip@bot.com",roles));
     }
 	
 }
